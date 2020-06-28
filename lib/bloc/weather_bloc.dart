39420e9b5084f66d47858551499f93a09c297e39
Yousef Roshandel <yousef.r1999@gmail.com>
2020-06-28 19:08:33 +0430

    implement bloc

--- lib/bloc/weather_bloc.dart
+++ lib/bloc/weather_bloc.dart
@@ -3,11 +3,16 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_fake_weather_app_bloc_stable/data/model/weather.dart';
+import 'package:flutter_fake_weather_app_bloc_stable/data/weather_repository.dart';
 
 part 'weather_event.dart';
 part 'weather_state.dart';
 
 class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
+  final WeatherRepository weatherRepository;
+
+  WeatherBloc(this.weatherRepository);
+
   @override
   WeatherState get initialState => WeatherInitial();
 
@@ -15,6 +20,22 @@ class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
   Stream<WeatherState> mapEventToState(
     WeatherEvent event,
   ) async* {
-    // TODO: implement mapEventToState
+    yield WeatherLoading();
+    if (event is GetWeather) {
+      try {
+        final weather = await weatherRepository.fetchWeather(event.cityName);
+        yield WeatherLoaded(weather);
+      } on NetworkError {
+        yield WeatherError("Couldn't fetch weather. is the device online?!");
+      }
+    } else if (event is GetDeatailedWeather) {
+      try {
+        final weather =
+            await weatherRepository.fetchDetailedWeather(event.cityName);
+        yield WeatherLoaded(weather);
+      } on NetworkError {
+        yield WeatherError("Counl't fetch weather. is the device online?!");
+      }
+    }
   }
 }
